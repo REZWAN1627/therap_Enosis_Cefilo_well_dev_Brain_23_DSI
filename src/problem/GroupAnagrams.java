035b8091da5191133bb621e548b2d4eaf8190d35
rezwankabir <rezwan.kabir@digigate360.com>
2023-08-28 13:54:09 +0600

    solved group anagram

--- src/problem/GroupAnagrams.java
+++ src/problem/GroupAnagrams.java
@@ -4,20 +4,22 @@ import java.util.*;
 
 public class GroupAnagrams {
     public static void main(String[] args) {
-        String[] arr = {"eat", "tea", "tan", "ate", "nat", "bat"};
-       // String[] arr = {"ac","c"};
-       // String[] arr = {"ac","d"};
+       //   String[] arr = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        String[] arr = {"cab", "tin", "pew", "duh", "may", "ill", "buy", "bar", "max", "doc"};
+        // String[] arr = {"ac","c"};
+        // String[] arr = {"ac","d"};
         List<List<String>> lists = groupAnagrams(arr);
         System.out.println(lists);
     }
 
     public static List<List<String>> groupAnagrams(String[] strs) {
-        Map<Integer, List<String>> listMap = new HashMap<>();
+        Map<String, List<String>> listMap = new HashMap<>();
         for (int i = 0; i < strs.length; i++) {
-            int key = 0;
+            char[] position = new char[26];
             for (int j = 0; j < strs[i].length(); j++) {
-                key += strs[i].charAt(j);
+                position[strs[i].charAt(j) - 'a']++;
             }
+            String key = new String(position);
             if (listMap.containsKey(key)) {
                 List<String> strings = listMap.get(key);
                 strings.add(strs[i]);
